329e6e54d8c994da5f03b51e1a51434db5590a89
adenm <aden.mckinney10@gmail.com>
2023-12-22 14:29:33 -0500

    successfully altered display.launch.py to display in humble rviz

--- sec_test_dev_humble_ws/src/sec_description/launch/display.launch.py
+++ sec_test_dev_humble_ws/src/sec_description/launch/display.launch.py
@@ -2,6 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import xacro
 
@@ -10,25 +11,24 @@ def generate_launch_description():
 
     # Specify the name of the package and path to xacro file within the package
     pkg_name = 'sec_description'
-    file_subpath = 'description/sec_description.urdf.xacro'
-    final_descriptor = 'description/sec_description_final.urdf.xacro'
+    file_subpath = 'description/sec_description.urdf.xml'
+    final_descriptor = 'description/sec_description_final.urdf.xml'
 
+    # use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
     # Use xacro to process the file
-    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
-    robot_description_raw = xacro.process_file(xacro_file).toprettyxml()
-
-    final_descriptor = os.path.join(get_package_share_directory(pkg_name),final_descriptor)
-
-    with open(final_descriptor, "w+") as file:
-        file.write(robot_description_raw)
+    pkg_path = os.path.join(get_package_share_directory('sec_description'))
+    xacro_file = os.path.join(pkg_path,'description','sec_description.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
 
     # Configure the node
     node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        arguments=[final_descriptor]
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_description_config.toxml()}],
     )
 
     node_joint_state_publisher_gui = Node(
@@ -44,6 +44,10 @@ def generate_launch_description():
 
     # Run the node
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
         node_robot_state_publisher,
         node_joint_state_publisher_gui,
         node_rviz2
