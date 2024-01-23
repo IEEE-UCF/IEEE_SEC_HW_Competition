b2e4ff4ad0a5d42151b83f4bc5041a580ededc3b
Patrick Rizkalla <patrickrizkalla@yahoo.com>
2024-01-23 01:11:27 -0500

    Adding Current OpenCV Files

---
+++ opencv/CMakeFiles/buildFile.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/buildFile.dir/photo.cpp.o"
+  "CMakeFiles/buildFile.dir/photo.cpp.o.d"
+  "buildFile"
+  "buildFile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/buildFile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
