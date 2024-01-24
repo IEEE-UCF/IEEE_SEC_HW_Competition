734fa6d83269f2df2a72d9736c1798efe7ecacb2
adenm <aden.mckinney10@gmail.com>
2024-01-23 22:11:04 -0500

    fixed world spawning inconsistencies

--- sec_test_dev_humble_ws/src/sec_description/launch/gazebo_world.launch.py
+++ sec_test_dev_humble_ws/src/sec_description/launch/gazebo_world.launch.py
@@ -5,9 +5,15 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
+    pkg_name = 'sec_description'
+    world_file_name = 'comp_course.world'
+
     # Get the directory of your ROS 2 package
-    pkg_dir = get_package_share_directory('sec_description')
-    world_file_path = os.path.join(get_package_share_directory(pkg_dir), 'worlds', 'comp_course.world')
+    pkg_dir = get_package_share_directory(pkg_name)
+    world_file_path = os.path.join(pkg_dir, 'worlds', world_file_name)
+
+    gazebo_model_path = os.path.join(get_package_share_directory(pkg_name), 'models')
+    set_model_path = SetEnvironmentVariable('GAZEBO_MODEL_PATH', gazebo_model_path)
 
     # Launch Gazebo with the specified world file
     start_gazebo_cmd = ExecuteProcess(
@@ -16,6 +22,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        set_model_path,
         start_gazebo_cmd
     ])
-
