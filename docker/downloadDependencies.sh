540b29bbbf5b6a0ead59387d75ab3a56bd689763
steve1442 <keller1442@gmail.com>
2024-03-01 16:53:39 -0500

    trying to reduce amount of internet needed

---
+++ docker/downloadDependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+source /opt/ros/humble/install/setup.bash
+
+pip3 install smbus
+pip install pyserial
+
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+
+sudo apt install -y ros-humble-rmw-cyclonedds-cpp
+cd /src_ws
