121065e1c9f0a116e47b0285e2cb0154266917ad
steve1442 <keller1442@gmail.com>
2024-03-02 00:47:42 -0500

    honestly idr

--- src_ws/src/secbot_bring_up/launch/nonav_control.launch.py
+++ src_ws/src/secbot_bring_up/launch/nonav_control.launch.py
@@ -98,11 +98,11 @@ def generate_launch_description():
         parameters=[ekf_params_file]
     )
 
-    start_imu
+ #   start_imu
 
-    start_lidar
+ #   start_lidar
 
-    start_camera
+ #   start_camera
 
     # ros2_control.xacro
     #   ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r /cmd_vel:=/diff_drive_controller/cmd_vel_unstamped
