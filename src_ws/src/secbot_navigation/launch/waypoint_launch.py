c45799b9374c24972686b308b64597ba4ecd0381
orionb05 <orionbrown05@gmail.com>
2024-03-05 16:56:36 -0500

    made a launch file that starts the server node before waypoint follower

---
+++ src_ws/src/secbot_navigation/launch/waypoint_launch.py
@@ -0,0 +1,25 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    waypoint_package = get_package_share_directory('secbot_navigation')
+
+    return LaunchDescription([
+        Node(
+            package='secbot_navigation',
+            namespace='',
+            executable='server',
+            name='server',
+        ),        
+        Node(
+            package='secbot_navigation',
+            namespace='',
+            executable='follow_waypoints.py',
+            name='follow_waypoints',
+        )
+    ])
