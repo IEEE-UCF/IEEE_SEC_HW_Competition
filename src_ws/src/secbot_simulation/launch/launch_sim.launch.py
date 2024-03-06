5c191f00d8288986c990f36ceb6c2fe0a0ce8b38
orionb05 <orionbrown05@gmail.com>
2024-03-05 19:25:45 -0500

    added ball tracker that can hopefully be executed at each waypoint. Need to integrate waypoint involvement

--- src_ws/src/secbot_simulation/launch/launch_sim.launch.py
+++ src_ws/src/secbot_simulation/launch/launch_sim.launch.py
@@ -54,7 +54,7 @@ def generate_launch_description():
 
     declare_use_gazebo_gui = DeclareLaunchArgument(
         name='use_gazebo_gui',
-        default_value='0',
+        default_value='true',
         description='Whether to launch Gazebo with or without GUI, default is without gui'
     )
 
