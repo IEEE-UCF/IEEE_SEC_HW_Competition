73df756edea5f1eaace362aca9e8050356660869
Patrick Rizkalla <patrickrizkalla@yahoo.com>
2024-03-16 22:19:51 -0400

    added camera connection from jetson to opencv

--- opencv_ws/build/image_subscriber/CMakeFiles/image_subscriber.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for image_subscriber.
-# This may be replaced when dependencies are built.
--- opencv_ws/src/image_publisher/src/image_publisher.cpp
+++ opencv_ws/src/image_publisher/src/image_publisher.cpp
@@ -2,6 +2,10 @@
 #include "image_transport/image_transport.hpp"
 #include "opencv2/core/mat.hpp"
 #include "opencv2/imgcodecs.hpp"
+#include <opencv2/opencv.hpp> 
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 int main(int argc, char ** argv)
@@ -12,13 +16,30 @@ int main(int argc, char ** argv)
   image_transport::ImageTransport it(node);
   image_transport::Publisher pub = it.advertise("camera/image", 1);
 
-  cv::Mat image = cv::imread(argv[1], cv::IMREAD_COLOR);
+  //cv::Mat image = cv::imread(argv[1], cv::IMREAD_COLOR);
+
+  cv::VideoCapture cap("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080,format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert !  appsink");
+
+  if ( !cap.isOpened() )  // if not success, exit program
+    {
+         std::cout << "Cannot open the web cam" << std::endl;
+         return -1;
+    }
+
   std_msgs::msg::Header hdr;
-  sensor_msgs::msg::Image::SharedPtr msg = cv_bridge::CvImage(hdr, "bgr8", image).toImageMsg();
+  cv::Mat image;
+  sensor_msgs::msg::Image::SharedPtr msg;
+  //sensor_msgs::msg::Image::SharedPtr msg = cv_bridge::CvImage(hdr, "bgr8", image).toImageMsg();
 
   rclcpp::WallRate loop_rate(5);
   while (rclcpp::ok()) {
+    std::cout << "hi" << std::endl;
+    cv::cap.read(image);
+    std::cout << "Cannot open the web cam" << std::endl;
+    msg = cv_bridge::CvImage(hdr, "bgr8", image).toImageMsg();
+    std::cout << "wow" << std::endl;
     pub.publish(msg);
+    std::cout << "maybe here?" << std::endl;
     rclcpp::spin_some(node);
     loop_rate.sleep();
   }
